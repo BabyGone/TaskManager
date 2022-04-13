7764fc5dcfbcb84e66b565bd28527ce58c621df4
BabyGone <90824775+BabyGone@users.noreply.github.com>
2022-04-13 17:02:43 +0700

    Add files via upload

---
+++ TaskManager/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace TaskManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstProcess = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnProperties = new System.Windows.Forms.Button();
+            this.btnStartTask = new System.Windows.Forms.Button();
+            this.btnEndTask = new System.Windows.Forms.Button();
+            this.btnGetProcess = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstProcess
+            // 
+            this.lstProcess.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(245)))), ((int)(((byte)(77)))));
+            this.lstProcess.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstProcess.FormattingEnabled = true;
+            this.lstProcess.ItemHeight = 20;
+            this.lstProcess.Location = new System.Drawing.Point(21, 78);
+            this.lstProcess.Name = "lstProcess";
+            this.lstProcess.Size = new System.Drawing.Size(400, 444);
+            this.lstProcess.Sorted = true;
+            this.lstProcess.TabIndex = 0;
+            this.lstProcess.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(83)))), ((int)(((byte)(83)))));
+            this.label1.Location = new System.Drawing.Point(261, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(333, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Trình quản lý tác vụ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(177)))), ((int)(((byte)(57)))));
+            this.panel1.Controls.Add(this.btnProperties);
+            this.panel1.Controls.Add(this.btnStartTask);
+            this.panel1.Controls.Add(this.btnEndTask);
+            this.panel1.Controls.Add(this.btnGetProcess);
+            this.panel1.Location = new System.Drawing.Point(438, 77);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(368, 444);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnProperties
+            // 
+            this.btnProperties.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnProperties.Location = new System.Drawing.Point(107, 298);
+            this.btnProperties.Name = "btnProperties";
+            this.btnProperties.Size = new System.Drawing.Size(166, 60);
+            this.btnProperties.TabIndex = 3;
+            this.btnProperties.Text = "Thông tin chi tiết";
+            this.btnProperties.UseVisualStyleBackColor = true;
+            this.btnProperties.Click += new System.EventHandler(this.btnProperties_Click);
+            // 
+            // btnStartTask
+            // 
+            this.btnStartTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnStartTask.Location = new System.Drawing.Point(107, 225);
+            this.btnStartTask.Name = "btnStartTask";
+            this.btnStartTask.Size = new System.Drawing.Size(166, 60);
+            this.btnStartTask.TabIndex = 2;
+            this.btnStartTask.Text = "Chạy tiến trình";
+            this.btnStartTask.UseVisualStyleBackColor = true;
+            this.btnStartTask.Click += new System.EventHandler(this.btnStartTask_Click);
+            // 
+            // btnEndTask
+            // 
+            this.btnEndTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnEndTask.Location = new System.Drawing.Point(107, 151);
+            this.btnEndTask.Name = "btnEndTask";
+            this.btnEndTask.Size = new System.Drawing.Size(166, 60);
+            this.btnEndTask.TabIndex = 1;
+            this.btnEndTask.Text = "Dừng tiến trình";
+            this.btnEndTask.UseVisualStyleBackColor = true;
+            this.btnEndTask.Click += new System.EventHandler(this.btnEndTask_Click);
+            // 
+            // btnGetProcess
+            // 
+            this.btnGetProcess.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnGetProcess.Location = new System.Drawing.Point(107, 81);
+            this.btnGetProcess.Name = "btnGetProcess";
+            this.btnGetProcess.Size = new System.Drawing.Size(166, 60);
+            this.btnGetProcess.TabIndex = 0;
+            this.btnGetProcess.Text = "Hiển thị tiến trình";
+            this.btnGetProcess.UseVisualStyleBackColor = true;
+            this.btnGetProcess.Click += new System.EventHandler(this.btnGetProcess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(63)))), ((int)(((byte)(107)))));
+            this.ClientSize = new System.Drawing.Size(829, 561);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstProcess);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.Name = "Form1";
+            this.Text = "Task Manager";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstProcess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnProperties;
+        private System.Windows.Forms.Button btnStartTask;
+        private System.Windows.Forms.Button btnEndTask;
+        private System.Windows.Forms.Button btnGetProcess;
+    }
+}
+
---
+++ TaskManager/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.VisualBasic;
+
+namespace TaskManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetProcess_Click(object sender, EventArgs e)
+        {
+            // Gọi hàm hiển thị tiến trình
+            GetProcesses();
+            // Làm mới danh sách tiến trình
+            lstProcess.Items.Clear();
+            // Gọi lại hàm hiển thị tiến trình
+            GetProcesses();
+        }
+
+        // Hàm hiển thị tiến trình
+        private void GetProcesses()
+        {
+            // Biến tiến trình
+            Process[] task = Process.GetProcesses();
+
+            // Vòng lặp đưa dữ liệu vào bảng ListBox
+            foreach (Process proc in task)
+            {
+                lstProcess.Items.Add(proc.ProcessName);
+            }           
+        }
+
+        // Hàm click chuột vào nút để dừng chương trình được chọn
+        private void btnEndTask_Click(object sender, EventArgs e)
+        {
+            // Biến tiến trình
+            Process[] task = Process.GetProcesses();
+
+            // Vòng lặp đọc dữ liệu
+            foreach (Process proc in task)
+            {
+                // Tiến trình được chọn sẽ được dừng lại
+                if(lstProcess.SelectedItem.ToString() == proc.ProcessName)
+                {
+                    // Dừng chương trình
+                    proc.Kill();
+                    // Kết thúc vòng lặp
+                    break;
+                }    
+            }
+            // Làm mới danh sách tiến trình
+            lstProcess.Items.Clear();
+            // Gọi lại hàm hiển thị tiến trình
+            GetProcesses();
+        }
+
+        // Hàm chạy tiến trình mới
+        private void btnStartTask_Click(object sender, EventArgs e)
+        {
+            // Tạo InputBox để nhập tên tiến trình cần chạy
+            string path = Microsoft.VisualBasic.Interaction.InputBox("Nhập tiến trình", "Task Manager", "", 350, 350);
+            // Khởi động tiến trình
+            Process.Start(path);
+            // Làm mới danh sách tiến trình
+            lstProcess.Items.Clear();
+            // Gọi lại hàm hiển thị tiến trình
+            GetProcesses();
+        }
+
+        // Hàm xem thông tin chi tiết của tiến trình
+        private void btnProperties_Click(object sender, EventArgs e)
+        {
+            // Biến tiến trình
+            Process[] task = Process.GetProcesses();
+
+            // Vòng lặp đọc dữ liệu
+            foreach (Process proc in task)
+            {
+                // Chọn tiến trình để xem thông tin chi tiết
+                if (lstProcess.SelectedItem.ToString() == proc.ProcessName)
+                {
+                    string temp = string.Empty;
+                    temp += "ID tiến trình : " + proc.Id.ToString(); // Lấy ID tiến trình được chọn
+                    temp += "\nThời gian sử dụng tiến trình : " + proc.UserProcessorTime.ToString(); // Lấy thời gian sử dụng tiến trình được chọn
+                    temp += "\nThời gian bắt đầu tiến trình : " + proc.StartTime.ToString(); // Lấy thời gian bắt đầu tiến trình được chọn
+                    MessageBox.Show(temp); // Hiện form thông tin chi tiết
+                    break; // Dừng vòng lặp
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
